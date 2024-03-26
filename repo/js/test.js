fca0dcc00061bde829f72b9d8e246021962c66c6
OConnorThomas <thomasorocks@gmail.com>
2024-03-26 15:22:40 -0400

    button hover design

--- repo/js/test.js
+++ repo/js/test.js
@@ -10,9 +10,11 @@ function updateContent() {
   if (get_logged_in() == 1) {
     usertarget.textContent = userName;
     accounttype.insertAdjacentHTML("afterend", "<a href=\"account.html\"><img src=\"repo/images/Generic-Profile-Image.png\"alt=\"Profile Picture\" height=\"60\"/></a>");
+    accounttype.insertAdjacentHTML("afterend", "<a href=\"settings.html\"><img src=\"repo/images/gear.png\" alt=\"Settings button\" height=\"40\"/></a>")
   } else {
     usertarget.textContent = "";
-    accounttype.insertAdjacentHTML("afterend", "<div class=\"white-borders\"><ul><li><a href=\"signup.html\" class=\"shadowed-text\">Signup</a></li></ul></div>");
+    accounttype.insertAdjacentHTML("afterend", "<div class=\"pad\"><ul><li><a href=\"signup.html\" class=\"shadowed-text invert-wb\">Signup</a></li></ul></div>");
+    accounttype.insertAdjacentHTML("afterend", "<div class=\"pad\"><ul><li><a href=\"login.html\" class=\"shadowed-text invert-wb\">Login</a></li></ul></div>");
   }
 }
 
