20ac7a2efcc35808e358d2c0634f0b1a6b5145f5
OConnorThomas <thomasorocks@gmail.com>
2024-03-27 14:30:26 -0400

    blur boxes

--- repo/js/test.js
+++ repo/js/test.js
@@ -1,6 +1,4 @@
-let usertarget = document.getElementById("userName");
 let accounttype = document.getElementById("accountbox")
-let userName = localStorage.getItem("userName");
 
 function get_logged_in() {
   return localStorage.getItem("_LOGGEDIN");
@@ -8,11 +6,9 @@ function get_logged_in() {
 
 function updateContent() {
   if (get_logged_in() == 1) {
-    usertarget.textContent = userName;
     accounttype.insertAdjacentHTML("afterend", "<a href=\"account.html\"><img src=\"repo/images/Generic-Profile-Image.png\"alt=\"Profile Picture\" height=\"60\"/></a>");
     accounttype.insertAdjacentHTML("afterend", "<a href=\"settings.html\"><img src=\"repo/images/gear.png\" alt=\"Settings button\" height=\"40\"/></a>")
   } else {
-    usertarget.textContent = "";
     accounttype.insertAdjacentHTML("afterend", "<div class=\"pad\"><ul><li><a href=\"signup.html\" class=\"shadowed-text invert-wb\">Signup</a></li></ul></div>");
     accounttype.insertAdjacentHTML("afterend", "<div class=\"pad\"><ul><li><a href=\"login.html\" class=\"shadowed-text invert-wb\">Login</a></li></ul></div>");
   }
