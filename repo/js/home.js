e9c2c625aaec5c3b8c107be6f878da73bc121242
OConnorThomas <thomasorocks@gmail.com>
2024-04-05 16:57:21 -0400

    converted to viewport relative measurements

--- repo/js/home.js
+++ repo/js/home.js
@@ -6,8 +6,8 @@ function get_logged_in() {
 
 function updateContent() {
   if (get_logged_in() == 1) {
-    accounttype.insertAdjacentHTML("afterend", "<a href=\"account.html\"><img src=\"repo/images/Generic-Profile-Image.png\"alt=\"Profile Picture\" height=\"60\"/></a>");
-    accounttype.insertAdjacentHTML("afterend", "<a href=\"settings.html\"><img src=\"repo/images/gear.png\" alt=\"Settings button\" height=\"40\"/></a>")
+    accounttype.insertAdjacentHTML("afterend", "<a href=\"account.html\"><img src=\"repo/images/Generic-Profile-Image.png\"alt=\"Profile Picture\" class=\"const-size\"/></a>");
+    accounttype.insertAdjacentHTML("afterend", "<a href=\"settings.html\"><img src=\"repo/images/gear.png\" alt=\"Settings button\" class=\"const-size settings-icon-size\"/></a>")
   } else {
     accounttype.insertAdjacentHTML("afterend", "<div class=\"pad\"><ul><li><a href=\"signup.html\" class=\"shadowed-text invert-wb\">Signup</a></li></ul></div>");
     accounttype.insertAdjacentHTML("afterend", "<div class=\"pad\"><ul><li><a href=\"login.html\" class=\"shadowed-text invert-wb\">Login</a></li></ul></div>");
