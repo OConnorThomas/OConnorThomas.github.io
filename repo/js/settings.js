deb46e38a5e6c8c8750c7577a1dac9d9ff3ad398
OConnorThomas <thomasorocks@gmail.com>
2025-02-08 17:37:11 -0500

    - old files, + viewport size standardization

--- repo/js/settings.js
+++
@@ -1,9 +0,0 @@
-
-const colorPicker = document.getElementById('colorPicker');
-
-function update_c1() {
-  const selectedColor = this.value;
-  document.documentElement.style.setProperty('--user-color-1', selectedColor);
-}
-
-colorPicker.addEventListener('input', update_c1());
